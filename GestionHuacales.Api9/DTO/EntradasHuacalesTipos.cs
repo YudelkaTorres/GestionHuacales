132c2fc508363c6503c52b1fa20bd32f394167a8
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-10-19 14:48:12 -0400

    Creando el DTO EntradasHuacalesTipos.

---
+++ GestionHuacales.Api9/DTO/EntradasHuacalesTipos.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GestionHuacales.Api9.DTO;
+public class EntradasHuacalesTiposDto
+{
+    public int TipoId { get; set; }
+    public string Descripción { get; set;} = string.Empty;
+    public int Existencia { get; set; } 
+}
