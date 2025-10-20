ac32abdabf1b6236ed6c70e41ab89089e471ca89
Yudelka Torres Amparo <yudelka_torres@ucne.edu.do>
2025-10-20 17:34:04 -0400

    Modificación al Program para ´poder abrir el Api.

--- GestionHuacales.Api9/Program.cs
+++ GestionHuacales.Api9/Program.cs
@@ -22,12 +22,24 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "GestionHuacales API V1");
+        c.RoutePrefix = string.Empty;
+    }); 
 }
-
-app.UseHttpsRedirection();
+else
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "GestionHuacales API V1");
+        c.RoutePrefix = string.Empty;
+    });
+}
+    app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
